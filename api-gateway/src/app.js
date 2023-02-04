d437c6ae46324d0ef909cb18e5740ff86cdaa89b
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-02-04 16:06:41 -0500

    Update API Gateway

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -3,16 +3,33 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const axios = require('axios');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { IS_DEV, StatusCode, services, rateLimitOptions } = require('./settings');
 
 function configureMiddlewares(app) {
     app.use(cors());
     app.use(rateLimit(rateLimitOptions));
+    configureProxies(app);
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(helmet());
     app.use(morgan("dev"));
+}
+
+function configureProxies(app) {
+    // Redirect requests to the corresponding services
+    services.forEach(service => {
+        console.log(`Proxying requests from ${service.path} to ${service.target}`);
+        app.use(service.path, createProxyMiddleware({
+            target: service.target,
+            changeOrigin: true,
+            pathRewrite: {
+                [`^${service.path}`]: '',
+            },
+            logLevel: IS_DEV ? 'debug' : 'info',
+        }));
+    });
 }
 
 const app = express();
@@ -25,24 +42,11 @@ app.get('/health-check', (_, res) => {
 
 // Checks if all services are up and running
 app.get('/health-check-all', async (_, res) => {
-    const promises = services.map(service => fetch(`${service.target}/health-check`));
+    const promises = services.map(service => axios.get(service.baseUrl + '/health-check'));
     const results = await Promise.allSettled(promises);
     const isAllUp = results.every(result => result.status === 'fulfilled');
     res.status(isAllUp ? StatusCode.OK : StatusCode.SERVICE_UNAVAILABLE).send(isAllUp ? 'OK' : 'NOT OK');
 });
-
-// Redirect requests to the corresponding services
-for (const service of services) {
-    console.log(`Proxying requests from ${service.path} to ${service.target}`);
-    app.use(service.path, createProxyMiddleware({
-        target: service.target,
-        changeOrigin: true,
-        pathRewrite: {
-            [`^${service.path}`]: '',
-        },
-        logLevel: IS_DEV ? 'debug' : 'info',
-    }));
-}
 
 // Start the server
 app.listen(PORT, () => {
--- api-gateway/src/settings.js
+++ api-gateway/src/settings.js
@@ -15,8 +15,14 @@ const rateLimitOptions = {
 /**
  * @typedef {Object} Service
  * @property {string} path - The path to proxy requests to the service.
+ * @property {string} baseUrl - The base URL of the service.
  * @property {string} target - The target URL of the service.
  */
+
+const CREATE_SESSION_SERVICE_BASE_URL = process.env.CREATE_SESSION_SERVICE_BASE_URL || 'http://localhost:3001';
+const GET_SESSION_SERVICE_BASE_URL = process.env.GET_SESSION_SERVICE_BASE_URL || 'http://localhost:3002';
+const CREATE_POST_SERVICE_BASE_URL = process.env.CREATE_POST_SERVICE_BASE_URL || 'http://localhost:3003';
+const GET_POSTS_SERVICE_BASE_URL = process.env.GET_POSTS_SERVICE_BASE_URL || 'http://localhost:3004';
 
 /**
  * @type {Service[]}
@@ -24,10 +30,26 @@ const rateLimitOptions = {
  */
 const services = [
     {
-        path: '/test',
-        target: 'http://localhost:3001/',
+        path: '/create-anonymous-session',
+        baseUrl: CREATE_SESSION_SERVICE_BASE_URL,
+        target: `${CREATE_SESSION_SERVICE_BASE_URL}/create-anonymous-session`,
     },
-]
+    {
+        path: '/get-session',
+        baseUrl: GET_SESSION_SERVICE_BASE_URL,
+        target: `${GET_SESSION_SERVICE_BASE_URL}/get-session`,
+    },
+    {
+        path: '/create-post',
+        baseUrl: CREATE_POST_SERVICE_BASE_URL,
+        target: `${CREATE_POST_SERVICE_BASE_URL}/create-post`,
+    },
+    {
+        path: '/get-posts',
+        baseUrl: GET_POSTS_SERVICE_BASE_URL,
+        target: `${GET_POSTS_SERVICE_BASE_URL}/get-posts`,
+    },
+];
 
 module.exports = {
     IS_DEV,
